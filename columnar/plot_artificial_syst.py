0ac714326d021fc65cb9e1049b2cdf404af62f72
llayer <llayer@cern.ch>
2021-10-12 10:16:21 +0200

    clean up repo

--- columnar/plot_artificial_syst.py
+++ columnar/plot_artificial_syst.py
@@ -21,13 +21,18 @@ def save_var(sample, name, var_name, bins = 20, xlow = 0., xup = 350, corr=None)
         scale_qcd = 9. * 0.73 * 1.1
         sample = sample[sample["train_flag"] == "test"]
         sample['weight'] = sample['btag_weight2'] * scale_qcd
-    elif name == "TTJets_signal":
-        sample['weight'] = sample['norm'] * sample['trigger_weight'] * sample['btag_weight1']
-        dummy = ROOT.TH1D("dummy", "dummy", bins, xlow, xup)
-        fill_hist(dummy, sample[var_name], weights = sample['weight'])
-        sample = sample[sample["train_flag"] == "test"]
     else:
-        sample['weight'] = sample['norm'] * sample['trigger_weight'] * sample['btag_weight1']
+        if corr is None:
+            sample['weight'] = sample['norm'] * sample['trigger_weight'] * sample['btag_weight1']
+        elif corr == "btag_up":
+            sample['weight'] = sample['norm'] * sample['trigger_weight'] * sample['btag_weight1_up']
+        elif corr == "btag_down":
+            sample['weight'] = sample['norm'] * sample['trigger_weight'] * sample['btag_weight1_down']        
+        if "TTJets_signal" in name:
+            dummy = ROOT.TH1D("dummy", "dummy", bins, xlow, xup)
+            fill_hist(dummy, sample[var_name], weights = sample['weight'])
+            #sample = sample[sample["train_flag"] == "test"]
+            sample = sample[sample["is_train"] == 0]
             
         
     series = sample[var_name]
@@ -38,15 +43,15 @@ def save_var(sample, name, var_name, bins = 20, xlow = 0., xup = 350, corr=None)
     else:
         fill_hist(hist, series, weights = weights)
         # Renorm the test templates used in the ml
-        if (name == "TTJets_signal"):
-            print("RENORM TEMPLATE")
+        if ("TTJets_signal" in name):
+            print("RENORM TEMPLATE", name)
             renorm_factor = float(dummy.Integral()) / float(hist.Integral())
             hist.Scale(renorm_factor)
             print(renorm_factor, hist.Integral(), dummy.Integral())
     #print s, hist.Integral()
     hist.Write()
 
-def vars_to_histos(samples, variables, file_path):
+def vars_to_histos(samples, variables, file_path, weight_syst=True):
     
     file = ROOT.TFile(file_path, 'recreate')
     for name, sample in samples.items():
@@ -70,6 +75,21 @@ def vars_to_histos(samples, variables, file_path):
             else:
                 print(var, name)
             save_var(sample, name, var["var_name"], var["bins"], var["xlow"], var["xup"])
+            if weight_syst == True:
+                for corr in ["btag_up", "btag_down"]:#, "trigger_up", "trigger_down", "xsec_up", "xsec_down"]:
+                    save_var(sample, name, var["var_name"], var["bins"], var["xlow"], var["xup"], corr = corr)
+
+            """
+            if "norm" in var["var_name"]:
+                if "bce" in var["var_name"]:
+                    save_var(sample, name, var["var_name"], var["bins"], var["xlow"], var["xup"], norm_name=norm_name)
+                else:
+                    idx = var["var_name"].split("_")[-1]
+                    norm_name = 
+                    save_var(sample, name, var["var_name"], var["bins"], var["xlow"], var["xup"], norm_name=norm_name)
+            else:
+                save_var(sample, name, var["var_name"], var["bins"], var["xlow"], var["xup"])
+            """
     file.Close()
     
     
@@ -113,21 +133,21 @@ def createCanvasPads():
 
     c = ROOT.TCanvas("c", "canvas", 800, 800)
     # Upper histogram plot is pad1
-    pad1 = ROOT.TPad("pad1", "pad1", 0, 0.51, 1, 1.0)
-    pad1.SetBottomMargin(0)  # joins upper and lower plot
+    pad1 = ROOT.TPad("pad1", "pad1", 0, 0.5, 1, 1.0)
+    pad1.SetBottomMargin(0.05)  # joins upper and lower plot
     #pad1.SetGridx()
     pad1.Draw()
     # Lower ratio plot is pad2
     c.cd()  # returns to main canvas before defining pad2
     pad2 = ROOT.TPad("pad2", "pad2", 0, 0.3, 1, 0.5)
-    pad2.SetTopMargin(0)  # joins upper and lower plot
+    pad2.SetTopMargin(0.05)  # joins upper and lower plot
     pad2.SetBottomMargin(0.3)
     #pad2.SetGridx()
     pad2.Draw()
 
     return c, pad1, pad2
     
-def plot_variation(outpath, var, xlabel, corr, cent, up, down):
+def plot_variation(outpath, var, xlabel, corr, cent, up, down, bce=False):
 
     ROOT.gROOT.SetBatch()
     ROOT.gStyle.SetOptStat(0)
@@ -136,59 +156,71 @@ def plot_variation(outpath, var, xlabel, corr, cent, up, down):
     ratio_down = createRatio(down, cent)
     
     c, pad1, pad2 = createCanvasPads()
+    #return c, pad1, pad2, ratio_up, ratio_down
 
-    # draw everything
     pad1.cd()
-    cent.Draw()
-    up.Draw("SAME")
+    cent.SetLineWidth(2)
+    up.Draw()
+    cent.Draw("SAME")
     up.SetLineColor(ROOT.kRed+2)
+    up.SetLineWidth(2)
+    up.SetTitle("")
+    up.GetYaxis().SetLabelSize(0.05)
+    up.GetXaxis().SetLabelSize(0.)
+    up.GetYaxis().CenterTitle()
+    up.GetYaxis().SetTitle("Events / bin")
+    up.GetYaxis().SetTitleSize(0.07)
+    up.GetYaxis().SetTitleOffset(0.7)
+    
+    if bce == False:
+        maximum = up.GetMaximum()
+        max_sf=1.5
+        up.SetMaximum(maximum*max_sf)
+        print("BCE", bce) 
+    
     down.Draw("SAME")
     down.SetLineColor(ROOT.kGreen+2)
+    down.SetLineWidth(2)
 
-    cent.SetTitle(var + " " + corr)
-    print(var, xlabel)
-    leg = ROOT.TLegend(0.6, 0.65, .8, .89);
+    #cent.SetTitle(var + " " + corr)
+    #print(var, xlabel)
+    leg = ROOT.TLegend(0.35, 0.65, .65, .89);
     leg.AddEntry(cent, "central", "l");
     leg.AddEntry(up, "up", "l");
     leg.AddEntry(down, "down", "l");
     leg.SetBorderSize(0)
+    leg.SetTextSize(0.07)
     leg.Draw("SAME")
-    
+
     pad2.cd()
     ratio_up.Draw("ep")
+    ratio_up.GetXaxis().SetTitle(xlabel)
     ratio_up.SetLineColor(ROOT.kRed+2)
+    ratio_up.SetLineWidth(2)
+    ratio_up.GetYaxis().SetTitle("Ratio")
+    ratio_up.GetYaxis().SetTitleSize(30)
+    ratio_up.GetYaxis().CenterTitle()
+    ratio_up.GetYaxis().SetTitleOffset(1.2)
+    ratio_up.GetXaxis().SetLabelSize(20)
+    ratio_up.GetXaxis().SetTitleSize(25)
     ratio_up.SetMarkerColor(ROOT.kRed+2)
     ratio_down.Draw("epSAME")    
     ratio_down.SetMarkerColor(ROOT.kGreen+2)
+    ratio_down.SetLineWidth(2)
     ratio_down.SetLineColor(ROOT.kGreen+2)
-    ratio_down.GetXaxis().SetTitle(xlabel)
-    
-    f1 = ROOT.TLine(0, 1., 1,1.)
+    #ratio_up.GetXaxis().SetTitle(xlabel)
+    #ratio_up.GetYaxis().SetTitleSize(0.05)
+    #ratio_up.GetXaxis().SetLabelSize(0.1)
+    ratio_up.GetYaxis().SetLabelSize(20)
+    ratio_up.GetYaxis().SetNdivisions(504)
+
+    x0 = ratio_up.GetXaxis().GetXmin()
+    x1 = ratio_up.GetXaxis().GetXmax()
+    f1 = ROOT.TLine(x0, 1., x1,1.)
     f1.SetLineColor(ROOT.kBlack)
     f1.SetLineStyle(ROOT.kDashed)
     f1.Draw("same")
-
-
-    """
-    c = ROOT.TCanvas()
-    ROOT.gStyle.SetOptStat(0)
-    cent.Draw()
-    up.Draw("SAME")
-    up.SetLineColor(ROOT.kRed+2)
-    down.Draw("SAME")
-    down.SetLineColor(ROOT.kGreen+2)
-
-    cent.SetTitle(var + " " + corr)
-    print(var, xlabel)
-    cent.GetXaxis().SetTitle(xlabel)
-
-    leg = ROOT.TLegend(0.6, 0.7, .89, .89);
-    leg.AddEntry(cent, "central", "l");
-    leg.AddEntry(up, "up", "l");
-    leg.AddEntry(down, "down", "l");
-    leg.SetBorderSize(0)
-    leg.Draw("SAME")
-    """
+    
     c.Print( outpath + "/" + var + "_" + corr + ".png")
     
     
